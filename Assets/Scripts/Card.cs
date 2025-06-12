105b2f7eab61f26f8ec064186210cf7b36498641
WONDER_PC\justi <gd70justin@vfs.com>
2025-06-11 22:19:13 -0700

    Cards render with their suit and rank. Player hand cards can be hovered over, selected, and played. Rudimentary tween animations.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,32 +1,133 @@
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.U2D;
 
 public class Card : MonoBehaviour
 {
     public int cardRank;
     public string cardSuit;
     public bool isAvailable;
-    public bool isSelected;
+    public bool isSelected = false;
     public bool isInHand = false;
+    private PlayerHand _playerHand;
+
+    [Header("Rendering")]
+    [SerializeField] private SpriteAtlas _atlas;
+    private MeshRenderer _meshRenderer;
+    private Material _material;
+    private Sprite _defaultTexture;
+    private Sprite _hoverTexture;
+
+    [Header("Animation")]
+    [SerializeField] private float _hoverDistance;
+    [SerializeField] private float _hoverTime;
+    [SerializeField] private Vector3 _playerCardPlacementPoint;
+    [SerializeField] private float _playerCardPlacementTime;
+    [SerializeField] private float _onArrangeHandTime;
+
+    private void Start()
+    {
+        // declare the hand
+        _playerHand = FindObjectOfType<PlayerHand>();
+
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _material = _meshRenderer.material;
+        RenderCard();
+    }
 
     // hovering over the card when it's in hand.
     private void OnMouseEnter()
     {
         if (isInHand)
         {
+            _material.mainTexture = _hoverTexture.texture;
+            AnimHoverUp();
+        }
+    }
 
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(isSelected == true)
+            {
+                _playerHand.PlaySelectedCard(this);
+            }
+            _playerHand.CheckSelectedCard();
+            isSelected = !isSelected;
         }
     }
 
     private void OnMouseExit()
     {
-        if (isInHand)
+        if (isInHand && isSelected == false)
         {
-            if(!isSelected)
-            {
+            _material.mainTexture = _defaultTexture.texture;
+            AnimHoverDown();
+        }
+    }
 
+    /// <summary>
+    /// Rendering the face up side of the cards
+    /// </summary>
+    private void RenderCard()
+    {
+        Sprite[] sprites = new Sprite[_atlas.spriteCount];
+        _atlas.GetSprites(sprites);
+
+        Debug.Log("Render Called");
+        // choose the corresponding card sprites
+        foreach (Sprite check in sprites)
+        {
+            string checkName = check.name.Replace("(Clone)", "");
+            string hover = $"{cardRank}_{cardSuit}_1";
+            string def = $"{cardRank}_{cardSuit}_2";
+
+            if (checkName == hover)
+            {
+                _hoverTexture = check;
+            }
+
+            if (checkName == def)
+            {
+                _defaultTexture = check;
+                _material.mainTexture = check.texture;
+                _material.mainTextureOffset = UVToOffset(check.uv);
+                _material.mainTextureScale = UVToScale(check.uv);
             }
         }
     }
 
+    private Vector2 UVToOffset(Vector2[] uv)
+    {
+        Vector2 offset = uv[2];
+        return offset;
+    }
+
+    private Vector2 UVToScale(Vector2[] uv)
+    {
+        Vector2 scale = uv[1] - uv[2];
+        return scale;
+    }
+
+    /// <summary>
+    /// Card animations
+    /// </summary>
+    public void AnimHoverUp()
+    {
+        transform.DOLocalMoveY(_hoverDistance, _hoverTime);
+    }
+    public void AnimHoverDown()
+    {
+        transform.DOLocalMoveY(0, _hoverTime);
+    }
+    public void AnimPlayToTable()
+    {
+        transform.DOMove(_playerCardPlacementPoint, _playerCardPlacementTime);
+    }
+    public void AnimOnArrangeHand(Vector3 newPosition,Quaternion newRotation, float delay)
+    {
+        transform.DOLocalMove(newPosition, _onArrangeHandTime).SetDelay(delay).SetEase(Ease.InOutCubic);
+        transform.DOLocalRotateQuaternion(newRotation, _onArrangeHandTime);
+    }
 }
--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -10,6 +10,8 @@ public class DeckManager : MonoBehaviour
 
     private void Start()
     {
+        _totalDeck = FindObjectsOfType<Card>();
+
         ShuffleCards();
     }
 
